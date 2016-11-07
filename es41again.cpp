f84a20e082ca1e638c61e43491cb7b0b27675edf
Gio <99c_dalio_giovanni@brunoserver.local>
2016-11-07 13:05:56 +0100

    esercizi in clasee 41

---
+++ es41again.cpp
@@ -0,0 +1,37 @@
+// DA LIO GIOVANNI IV C 7/11/2016
+
+#include <iostream>
+#include <string.h>
+ using namespace std;
+
+ string nome;
+ int daily_money;
+ int tot_money=0;
+ int i=0;
+ int medium_money;
+
+ void acquisizione(int daily_money, int tot_money)
+ {cin>>nome;
+ cin>>daily_money;
+ tot_money=tot_money+daily_money;
+ i++;
+ }
+
+ void media( int medium_money, int tot_money, int i){
+ medium_money= tot_money/i;}
+
+
+ int main(){
+ char r;
+
+ while(r='s'){
+    cout<<"Inserisci il nome del reparto:";
+    cin>>nome;
+    cout<<"Inserisci il guadagno giornaliero:";
+    cin>>daily_money;
+    void ()
+ }
+
+
+
+ return 0;}

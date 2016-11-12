44a815ecebef9c92557070ff3d1524aabcb1a10f
Gio <99c_dalio_giovanni@brunoserver.local>
2016-11-12 12:42:16 +0100

    ok

--- es43.cpp
+++ es43.cpp
@@ -14,8 +14,8 @@
 
  void seggio(int voti_candidato_1, int voti_candidato_2){
  voti_totali=voti_candidato_1+voti_candidato_2;
- percentuale_candidato_1=(voti_candidato_1*100)/voti_totali;
- percentuale_candidato_2=(voti_candidato_2*100)/voti_totali;
+ percentuale_candidato_1=(float)(voti_candidato_1*100)/voti_totali;
+ percentuale_candidato_2=(float)(voti_candidato_2*100)/voti_totali;
  }
 
 int main(){
@@ -30,13 +30,13 @@ cin>>voti_candidato_2;
 seggio(voti_candidato_1, voti_candidato_2);
 if (percentuale_candidato_1>percentuale_candidato_2)
 {cout<<"Ha vinto "<<candidato_1<<" con il "<<percentuale_candidato_1<<" % dei voti"<<'\n';
- cout<<candidato_2<<"con il "<<percentuale_candidato_2<<" % dei voti e' arrivato al secondo posto";}
+ cout<<candidato_2<<" con il "<<percentuale_candidato_2<<" % dei voti e' arrivato al secondo posto";}
 else{
 if(percentuale_candidato_1==percentuale_candidato_2)
 {cout<<"I candidati hanno pareggiato con il "<<percentuale_candidato_1<<" % dei voti a testa";
 }else{
 cout<<"Ha vinto "<<candidato_2<<" con il "<<percentuale_candidato_2<<" % dei voti"<<'\n';
-cout<<candidato_1<<"con il "<<percentuale_candidato_1<<" % dei voti e' arrivato al secondo posto";
+cout<<candidato_1<<" con il "<<percentuale_candidato_1<<" % dei voti e' arrivato al secondo posto";
 }}
 return 0;
 }

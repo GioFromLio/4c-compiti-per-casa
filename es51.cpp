5f72a41cd49d76b7a9df32eb227fa85e34d94f9b
Gio <99c_dalio_giovanni@BRUNOSERVER.LOCAL>
2016-11-28 13:04:44 +0100

    felizzz

--- es51.cpp
+++ es51.cpp
@@ -8,11 +8,14 @@ int main(){
 
 int numeri[10];
 int i, f=0;
-cout<<"Inserisci 10 numeri"<<endl;
+char car = 2;
+
+cout<<"Inserisci 10 numeri: "<<endl;
 for(i=0; i<10; i++){
     cin>>numeri[i];
     if(numeri[i]>5){f++;}
     else{};
 }
-cout<<"I numeri maggiori di 5 sono in tutto: "<<f;
+cout<<"I numeri maggiori di 5 sono in tutto: "<<f<<endl;
+cout<<"Grazie per aver lavorato con noi"<<car;
 return 0;}

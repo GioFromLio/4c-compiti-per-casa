74cc41d55806cb9c22cce4333401995e961c8cf2
Gio <99c_dalio_giovanni@BRUNOSERVER.LOCAL>
2017-01-28 12:59:51 +0100

    21212121

--- es11.cpp
+++ es11.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
 #define N_SCUOLE 5
-
+#define N_STUDENTI 10
 using namespace std;
 
 class Scuola{
 public:
     unsigned int id;
     string denominazione;
+    unsigned int num_studenti;
+    studente studenti[N_STUDENTI];
     public:
         Scuola(string nome_scuola);
-        Scuola(string nome_scuola, unsigned int id);
+        Scuola(string nome_scuola, unsigned int id_scuola);
     };
 
 class Studente{
@@ -32,12 +34,27 @@ for (int i = 0; i<N_SCUOLE; i++){
     Scuola s(nome_scuola);
     scuole[i] = s;
     s.id= i;
+    char scelta = 's';
 
+while (scelta == 's'){
+        string nome_studente;
+    cout<<"Inserisci il nome dello studente: " << endl;
+    cin>>nome_studente;
+    string cognome_studente;
+    cout<<"Inserisci il cognome dello studente: " << endl;
+    cin>>cognome_studente;
+    Studente stud;
+    stud.nome = nome_studente;
+    stud.cognome= cognome_studente;
+    stud.id_scuola = i;
+    cout<<"Vuoi continuare?(s/n): " << endl;
+    cin>> scelta;
+}
 }
 return 0;
 }
 
-Scuola(string nome_scuola, unsigned int id){
+Scuola(string nome_scuola, unsigned int id_scuola){
 denominazione = nome_scuola;
 id=id_scuola;}
 
